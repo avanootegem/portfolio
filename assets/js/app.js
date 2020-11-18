7b499585be5e18b69b663cea9e0ca50266a8ba1d
avanootegem <aurelien.vanootegem@gmail.com>
2020-11-18 15:42:56 +0100

    Debut des anim

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+const blockName = document.getElementById("blockName");
+const navbar = document.querySelector("nav");
+
+let main_blue = "rgb(40,140,200)";
+setTimeout(() => {
+    blockName.style.transition = "2s";
+    blockName.style.transform = "translateX(100%)";
+
+    setTimeout(() => {
+        const navChilds = navbar.childNodes;
+        let i = 0;
+        setInterval(() => {
+            const element = navChilds[i];
+            switch (i) {
+                case 1:
+                    element.style.transition = "1s";
+                    element.style.opacity = "1";
+                    i++;
+                    break;
+
+                case 3:
+                    const ulChilds = element.childNodes;
+                    for (let j = 0; j < ulChilds.length; j++) {
+                        if (j % 2 === 1) {
+                            const li = ulChilds[j];
+                            li.style.transition = "1s";
+                            li.style.opacity = "1";
+                        }
+                    }
+                    i++;
+                    break;
+
+                default:
+                    i++
+                    break;
+            }
+
+            if (i >= navChilds.length) {
+                clearInterval();
+            } else {
+                console.log(i);
+            }
+        }, 500);
+        navbar.style.transition = "1s";
+        navbar.style.borderRightColor = main_blue;
+    }, 1000);
+}, 1000);
