97ea1e65c3ce3b1b2dad87e6508bcde6975068c4
avanootegem <aurelien.vanootegem@gmail.com>
2020-12-04 14:35:24 +0100

    Page2Prete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
+const html = document.querySelector("html");
 const blockName = document.getElementById("blockName");
+const blockBtn = document.getElementById("blockBtn");
 const blockSpecial = document.getElementById("blockSpeciality");
 const navbar = document.querySelector("nav");
 
@@ -7,71 +9,80 @@ setTimeout(() => {
     blockName.style.transition = "2s";
     blockName.style.transform = "translateX(100%)";
 
+
     setTimeout(() => {
-        blockSpecial.style.transition = "2s";
-        blockSpecial.style.transform = "translateY(100%)";
-
-        const span1 = document.getElementById("trait1"),
-            span2 = document.getElementById("trait2"),
-            span3 = document.getElementById("trait3"),
-            span4 = document.getElementById("trait4");
+        blockBtn.style.transition = "2s";
+        blockBtn.style.transform = "translateY(100%)";
 
         setTimeout(() => {
-            span1.style.transition = "1s";
-            span1.style.transform = "translateX(0)";
+            blockSpecial.style.transition = "2s";
+            blockSpecial.style.transform = "translateY(100%)";
 
-            span2.style.transition = "1s";
-            span2.style.transform = "translateY(0)";
+            const span1 = document.getElementById("trait1"),
+                span2 = document.getElementById("trait2"),
+                span3 = document.getElementById("trait3"),
+                span4 = document.getElementById("trait4");
 
-            span3.style.transition = "1s";
-            span3.style.transform = "translateX(0)";
+            setTimeout(() => {
+                span1.style.transition = "1s";
+                span1.style.transform = "translateX(0)";
 
-            span4.style.transition = "1s";
-            span4.style.transform = "translateY(0)";
-        }, 1200);
+                span2.style.transition = "1s";
+                span2.style.transform = "translateY(0)";
 
-        setTimeout(() => {
-            const navChilds = navbar.childNodes;
-            let i = 0;
-            let iInterval = setInterval(() => {
-                const element = navChilds[i];
-                switch (i) {
-                    case 1:
-                        element.style.transition = "1s";
-                        element.style.opacity = "1";
-                        i++;
-                        break;
+                span3.style.transition = "1s";
+                span3.style.transform = "translateX(0)";
 
-                    case 3:
-                        const ulChilds = element.childNodes;
-                        let j = 0;
-                        let jInterval = setInterval(() => {
-                            if (j % 2 === 1) {
-                                const li = ulChilds[j];
-                                li.style.transition = "1s";
-                                li.style.opacity = "1";
-                            }
-                            j++;
+                span4.style.transition = "1s";
+                span4.style.transform = "translateY(0)";
+            }, 1200);
 
-                            if (j >= ulChilds.length) {
-                                clearInterval(jInterval);
-                            }
-                        }, 200);
-                        i++;
-                        break;
+            setTimeout(() => {
+                const navChilds = navbar.childNodes;
+                let i = 0;
+                let iInterval = setInterval(() => {
+                    const element = navChilds[i];
+                    switch (i) {
+                        case 1:
+                            element.style.transition = "1s";
+                            element.style.opacity = "1";
+                            i++;
+                            break;
 
-                    default:
-                        i++
-                        break;
-                }
+                        case 3:
+                            const ulChilds = element.childNodes;
+                            let j = 0;
+                            let jInterval = setInterval(() => {
+                                if (j % 2 === 1) {
+                                    const li = ulChilds[j];
+                                    li.style.transition = "1s";
+                                    li.style.opacity = "1";
+                                }
+                                j++;
 
-                if (i >= navChilds.length) {
-                    clearInterval(iInterval);
-                }
-            }, 250);
-            navbar.style.transition = "1s";
-            navbar.style.borderRightColor = main_blue;
-            moveTheBlock();
+                                if (j >= ulChilds.length) {
+                                    clearInterval(jInterval);
+                                }
+                            }, 200);
+                            i++;
+                            break;
+
+                        default:
+                            i++
+                            break;
+                    }
+
+                    if (i >= navChilds.length) {
+                        clearInterval(iInterval);
+                    }
+                }, 250);
+                navbar.style.transition = "1s";
+                navbar.style.borderRightColor = main_blue;
+                moveTheBlock();
+                setTimeout(() => {
+                    html.style.overflowY = "scroll";
+                }, 2000);
+            }, 1000);
         }, 500);
     }, 1000);
 }, 1000);
