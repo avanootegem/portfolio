33410a6c22afd82fa66b81c44564164dcc26a862
avanootegem <aurelien.vanootegem@gmail.com>
2020-12-07 17:00:23 +0100

    JsProjectTerminé

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,9 +4,8 @@ function moveTheBlock() {
         span3 = document.getElementById("trait3"),
         span4 = document.getElementById("trait4");
 
-    console.log(span1);
     let posBlock = 0;
-    let moveBlock = setInterval(() => {
+    setInterval(() => {
             if(posBlock % 2 === 0) {
                 span1.style.transform = "translateX(-"+((2/3)*100+"%)");
                 span2.style.transform = "translateY("+((2/3)*100+"%)");
@@ -21,4 +20,36 @@ function moveTheBlock() {
                 posBlock ++;
             }
     }, 2000);
+}
+
+function createProject(array) {
+    const project = document.getElementById("project");
+    const theProject = document.getElementById("theProject")
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        const blockProject = document.createElement("div");
+        blockProject.classList.add("projet", "projet"+(i+1));
+        theProject.appendChild(blockProject);
+
+        const explication = document.createElement("div");
+        explication.classList.add("explication");
+        explication.append(element[0]);
+        blockProject.appendChild(explication);
+
+        const lien = document.createElement("a");
+        lien.href = element[1];
+        blockProject.appendChild(lien);
+
+        const image = document.createElement("img");
+        image.src = element[2];
+        image.alt = element[3];
+        lien.appendChild(image)
+        
+        project.style.height = (40*(i+1))+"vh";
+    }
+}
+
+function addProject(text, href, src, name) {
+    let array = [text, href, src, name];
+    arrayProject.push(array);
 }
---
+++ assets/js/project.js
@@ -0,0 +1,15 @@
+let arrayProject = [];
+
+let text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum eius officia dolore inventore.Repudiandae eveniet doloribus distinctio amet provident modi esse quae! Accusamus hic, ullam dolorem soluta officiis blanditiis unde.";
+let href = "";
+let src = "";
+let name = "Projet 1";
+addProject(text, href, src, name);
+
+name = "Projet 2";
+addProject(text, href, src, name);
+
+name = "Projet 3";
+addProject(text, href, src, name);
+
+createProject(arrayProject);
