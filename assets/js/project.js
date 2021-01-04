53738488b28499fd6feac8e7b2f9c91998669933
avanootegem <aurelien.vanootegem@gmail.com>
2021-01-04 13:54:00 +0100

    Correction Ortographe

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,16 +1,16 @@
 let arrayProject = [];
 
-let text = "Premier projet réalisé durant la formation, qui est une simple intégration Html / Css, avec utilisation de Bootstrap";
+let text = "Premier projet réalisé durant la formation.Intégration Html / Css, avec utilisation de Bootstrap";
 let href = "https://aurelienv.promo-45.codeur.online/Integration_Mark/";
 let src = "url('assets/img/mark.png')";
 addProject(text, href, src);
 
-text = "Projet d'introduction au Php, utlie pour pouvoir reviser ses tables de multiplications"
+text = "Projet d'introduction au Php, utile pour pouvoir réviser ses tables de multiplications"
 href = "https://aurelienv.promo-45.codeur.online/ProjetMultiplication/"
 src = "url('assets/img/multiplication.png')";
 addProject(text, href, src);
 
-text = "Projet d'introduction au CRUD, petit site permettant suite a une connexion, de proposer des idéées de Films"
+text = "Projet d'introduction au CRUD. Site permettant suite a une connexion, de proposer des idéées de Films"
 href = "https://aurelienv.promo-45.codeur.online/ExoSqlFilms/"
 src = "url('assets/img/crud.png')";
 addProject(text, href, src);
