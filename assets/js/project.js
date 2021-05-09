97964fac281e636b6f151c231347831d853f0e8c
avanootegem <aurelien.vanootegem@gmail.com>
2021-05-09 17:27:31 +0200

    clear

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,47 +1,47 @@
 let arrayProject = [];
 
 let text = "Premier projet réalisé durant la formation. Intégration Html / Css, avec utilisation de Bootstrap";
-let href = "https://aurelienv.promo-45.codeur.online/Integration_Mark/";
+let href = "/integration_mark/";
 let src = "url('assets/img/mark.png')";
 let srcOp = "url('assets/img/markOp.png')";
 addProject(text, href, src, srcOp);
 
 
 text = "Projet d'introduction à la fonctionnalité des flexbox, 2ème partie de l'intégration Css";
-href = "https://aurelienv.promo-45.codeur.online/Integration_Italianfood/";
+href = "/integration_italianfood/";
 src = "url('assets/img/italian.png')";
 srcOp = "url('assets/img/italianOp.png')";
 addProject(text, href, src, srcOp);
 
 
 text = "Projet d'introduction au Php, utile pour pouvoir réviser ses tables de multiplications";
-href = "https://aurelienv.promo-45.codeur.online/ProjetMultiplication/";
+href = "/multiplications/";
 src = "url('assets/img/multiplication.png')";
 srcOp = "url('assets/img/multiplicationOp.png')";
 addProject(text, href, src, srcOp);
 
 
 text = "Petit jeu en JavaScript, qui est basé sur le principe du jeu bomberman";
-href = "https://aurelienv.promo-45.codeur.online/ProjetBomberman/";
+href = "/bombermanlike/";
 src = "url('assets/img/bomberman.png')";
 srcOp = "url('assets/img/bombermanOp.png')";
 addProject(text, href, src, srcOp);
 
 
 text = "Projet d'introduction au CRUD. Site permettant suite a une connexion, de proposer des idées de Films";
-href = "https://aurelienv.promo-45.codeur.online/ExoSqlFilms/";
+href = "/ExoSqlFilms/";
 src = "url('assets/img/crud.png')";
 srcOp = "url('assets/img/crudOp.png')";
 addProject(text, href, src, srcOp);
 
 text = "Projet avec utilisation de mvc, recensant différents films";
-href = "https://aurelienv.promo-45.codeur.online/projet_mvc";
+href = "/projet_mvc";
 src = "url('assets/img/mvc.png')";
 srcOp = "url('assets/img/mvcOp.png')";
 addProject(text, href, src, srcOp);
 
 text = "Projet carte de voeux, réalisé pour les fêtes de fin d'année";
-href = "https://aurelienv.promo-45.codeur.online/CartDeVoeux";
+href = "/CartDeVoeux";
 src = "url('assets/img/carte.png')";
 srcOp = "url('assets/img/carteOp.png')";
 addProject(text, href, src, srcOp);
